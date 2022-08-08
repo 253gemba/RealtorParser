00cfe3b1bed48f7c56b23c414a4c441137834898
Charlotte000 <ramil.mifteev@yandex.ru>
2022-08-09 01:10:49 +0300

    add gunicorn & nginx

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,11 @@ ENV PYTHONUNBUFFERED=1
 RUN apt update
 RUN apt install tesseract-ocr -y
 
-WORKDIR /app
+
+RUN mkdir -p /home/app
+RUN mkdir -p /home/app/app/staticfiles
+WORKDIR /home/app/app
+
 COPY ./requirements.txt .
 RUN pip install -r ./requirements.txt
 COPY . .
--- RealtorParser/settings.py
+++ RealtorParser/settings.py
@@ -28,6 +28,8 @@ DEBUG = False
 
 ALLOWED_HOSTS = [ '*' ]
 
+CSRF_TRUSTED_ORIGINS = ['http://localhost:1337']
+
 
 # Application definition
 
@@ -138,8 +140,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

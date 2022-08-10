99e56739030d34d5c691570610005f33a4580033
roma <trueget33@gmail.com>
2022-08-10 16:16:50 +0300

    добил юлу но номер вытащить так и не получилось, оставил 0 вместо номера

--- youla_parcer.py
+++ youla_parcer.py
@@ -6,15 +6,16 @@ print()
 print('НАЧАЛО')
 print()
 
-# def record_data(data):
-#     list_data = [data]
-#     print(list_data)
-#     return list_data
+def record_data(data):
+    list_data = []
+    list_data.append(data)
+    print(data)
+    return list_data
 
 
 def get_links():
     links = []
-    url = 'https://youla.ru/moskva/nedvijimost?attributes[sort_field]=date_published'
+    url = 'https://youla.ru/moskva/nedvijimost/prodaja-kvartiri?attributes[realty_building_type][0]=166228&attributes[term_of_placement][from]=-1%20day&attributes[term_of_placement][to]=now'
     driver = webdriver.Chrome()
     driver.get(url)
     time.sleep(3)
@@ -39,15 +40,44 @@ def get_links():
 def get_data():
     new_links = get_links()
     print(len(new_links))
-    for lin in new_links:
+    for card_link in new_links:
         driver = webdriver.Chrome()
-        driver.get(lin)
+        driver.get(card_link)
         time.sleep(1)
-        adress = driver.find_element(By.CLASS_NAME, 'sc-eWuggI').find_element(By.TAG_NAME, 'span').text
-        price = driver.find_element(By.CLASS_NAME, 'sc-dbGQSH').find_element(By.TAG_NAME, 'span').text
+        name = driver.find_element(By.CLASS_NAME, 'sc-mlOqW').find_element(By.TAG_NAME, 'a').text.split('(')[0]
 
+        adress = driver.find_element(By.CLASS_NAME, 'sc-eWuggI').find_element(By.TAG_NAME, 'span').text
+
+        floors = driver.find_elements(By.CLASS_NAME, 'dyaAgt')[1].find_elements(By.CLASS_NAME, 'sc-cOajty')
+        floor, max_floor = None, None
+
+        for i in range(len(floors)):
+            if 'Этаж' in floors[i].text:
+                floor = int(floors[i + 1].text)
+                break
+
+        for i in range(len(floors)):
+            if 'Этажность дома' in floors[i].text:
+                max_floor = int(floors[i + 1].text.split('\n')[0])
+                break
+
+        price = driver.find_element(By.CLASS_NAME, 'sc-dbGQSH').find_element(By.TAG_NAME, 'span').text\
+            .replace('\u205f', '')
+
+        link_img = driver.find_element(By.CLASS_NAME, 'sc-cKFVac').find_element(By.TAG_NAME, 'img').get_attribute('src')
+
+        area = driver.find_element(By.CLASS_NAME, 'sc-CARIn').find_element(By.TAG_NAME, 'h2').text.split(',')[-1]
+        area = area.replace(' м²', '').replace(' ', '')
+        if '.' in area:
+            area = float(area)
+        else:
+            area = int(area)
+
+        driver.quit()
+
+        record_data([name, 0, price, adress, area, floor, max_floor, card_link, link_img])
     return len(new_links)
 
 
-get_data()
-
+if __name__ == '__main__':
+    get_data()
